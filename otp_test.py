6b69956eb2cfefa02936dce9ac29bc67bf3fec58
distroter <guptarishi123455@gmail.com>
2018-11-18 12:00:03 +0530

    fourth commit

--- otp_test.py
+++ otp_test.py
@@ -5,9 +5,8 @@ Created on Fri Nov 16 21:37:20 2018
 @author: distroter
 """
 import pyotp,time
-totp = pyotp.TOTP("JBSWY3DPEHPK3PXP")
-totp.at(time.time(),1)
-totp.now()
-str=totp.now()
-time.sleep(16)
-totp.verify(str)
+secret = pyotp.random_base32()
+totp = pyotp.TOTP(secret,interval=60)
+otp = totp.now() 
+print(otp)
+totp.verify('415082')
