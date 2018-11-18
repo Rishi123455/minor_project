e80837309ac7075c6ee9945734da14bb57f95bfe
distroter <guptarishi123455@gmail.com>
2018-11-18 12:02:10 +0530

    fourth commit

--- test.py
+++ test.py
@@ -14,24 +14,43 @@ def hello():
     ECID = request.form['ECID'] 
     EPW = request.form['EPW']
     key = request.form['key']
+    choice = request.form['choice']
     
     key = key.encode('utf-8')
     print(key)
-    
+    choice = int(choice)
     cipher_suite = Fernet(key)
     
     DCID = cipher_suite.decrypt(ECID.encode('utf-8'))
     DPW = cipher_suite.decrypt(EPW.encode('utf-8'))
+    
     conn = MySQLdb.connect(user="root", passwd="rishi26071997", db="rishi")
     cur = conn.cursor()
-
-    cur.execute("insert into iot values ( %s , %s )",(DCID.decode('utf-8'),DPW.decode('utf-8')))
+    ans=""
+    if choice == 1 :
+        cur.execute("insert into iot values ( %s , %s )",(DCID.decode('utf-8'),DPW.decode('utf-8')))
+        ans = "Registered"
+    elif choice == 2 :
+        sql='SELECT PW FROM iot WHERE CID IN %s'
+        args=[[DCID.decode('utf-8')]]
+        cur.execute(sql,args)
+        row = cur.fetchone()
+        if DPW.decode('utf-8') == str(row[0]):
+            ans = "Matched" 
+        else:
+            ans ="Not Matched" 
     conn.commit()
     cur.close()
-    conn.close()
-    return DCID
-    
-    
+    conn.close()    
+    return ans
+        
+               
+        
+
+        
+        
+        
+            
 
 if __name__ == "__main__":
     app.run(debug=True,host="localhost",port=5000)
--- test3.py
+++ test3.py
@@ -8,9 +8,12 @@ Created on Wed Oct 10 13:09:10 2018
 from flask import Flask
 import requests
 from cryptography.fernet import Fernet
-print('Enter Username')
+print('Welcome to Registration portel')
+print('\'1\' for Registration \n\'2\' for Login ')
+choice=int(input())
+print('Enter your Customer ID')
 CID=input()
-print('Enter password')
+print('Enter your Password')
 PW=input()
 key = Fernet.generate_key()
 cipher_suite = Fernet(key)
@@ -18,5 +21,5 @@ cipher_suite = Fernet(key)
 encoded_CID = cipher_suite.encrypt(CID.encode('utf-8'))
 encoded_PW = cipher_suite.encrypt(PW.encode('utf-8'))
 # sending get request
-r = requests.post("http://localhost:5000", data={'ECID': encoded_CID,'EPW':encoded_PW,'key':key})
+r = requests.post("http://localhost:5000", data={'ECID': encoded_CID,'EPW':encoded_PW,'key':key,'choice':choice})
 print(r.text)
